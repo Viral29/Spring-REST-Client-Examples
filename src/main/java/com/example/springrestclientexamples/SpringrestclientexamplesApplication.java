6771aa88a81bc735c74305cdb0580101cbe99ef8
Viral29 <viru2994@gmail.com>
2018-06-19 02:15:58 -0400

    Initial commit

---
+++ src/main/java/com/example/springrestclientexamples/SpringrestclientexamplesApplication.java
@@ -0,0 +1,12 @@
+package com.example.springrestclientexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestclientexamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrestclientexamplesApplication.class, args);
+    }
+}
